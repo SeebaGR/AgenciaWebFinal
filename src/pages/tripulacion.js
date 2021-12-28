ca6008161a2ac56baa1e1d6be919160b871f61b1
Sebastian Godoy Rubio <s.godoy.rubio@gmail.com>
2021-12-28 11:39:55 -0300

    cambioss
    sadsdsad

--- src/pages/tripulacion.js
+++ src/pages/tripulacion.js
@@ -3,11 +3,7 @@ import React from "react";
 import Header from "../componentes/header"
 import SeccionTri1 from "../componentes/componentes_tripulacion/seccion_tri1"
 import SeccionTri2 from "../componentes/componentes_tripulacion/seccion_tri2"
-import SeccionTri3 from "../componentes/componentes_tripulacion/seccion_tri3"
-import SeccionTri4 from "../componentes/componentes_tripulacion/seccion_tri4"
-import SeccionTri5 from "../componentes/componentes_tripulacion/seccion_tri5"
-import SeccionTri6 from "../componentes/componentes_tripulacion/seccion_tri6"
-import SeccionTri7 from "../componentes/componentes_tripulacion/seccion_tri7"
+
 import Footer from "../componentes/footer";
 import SectionVideo from "../componentes/SectionVideo";
 import SectionSumate from "../componentes/componentes_home/sectionSumate";
@@ -23,12 +19,9 @@ const tripulacion = () => {
       <Header colorFondo={colorFondoHeader}></Header>
       <SeccionTri1></SeccionTri1>
       <SeccionTri2></SeccionTri2>
-      <SeccionTri3></SeccionTri3>
+     
       <SectionVideo colorFondo={colorFondo}></SectionVideo>
-      <SeccionTri4></SeccionTri4>
-      <SeccionTri5></SeccionTri5>
-      <SeccionTri6></SeccionTri6>
-      <SeccionTri7></SeccionTri7>
+
       <SectionSumate></SectionSumate>
       <SeparadorOlasSirena></SeparadorOlasSirena>
       <SectionSaberMas></SectionSaberMas>

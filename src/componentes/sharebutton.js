209d6a520c4332167d8c2e5a64a68ff5f42777b3
Sebastian Godoy Rubio <s.godoy.rubio@gmail.com>
2022-01-30 20:31:20 -0300

    Update sharebutton.js

--- src/componentes/sharebutton.js
+++ src/componentes/sharebutton.js
@@ -18,7 +18,7 @@ const ShareButtons = ({title, url, twitterHandle, tags, image, description}) => 
     return(
         <div>
                    
-                   <FacebookShareButton url={url}  title={title}>
+                   <FacebookShareButton url={url}  quote={title}>
                 <FacebookIcon  size={40} />
          </FacebookShareButton>
 

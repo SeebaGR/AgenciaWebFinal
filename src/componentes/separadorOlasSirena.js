c217f241ef545d6a2def7520c6925adc724ea03e
Sebastian Godoy Rubio <s.godoy.rubio@gmail.com>
2022-02-03 08:48:07 -0300

    l

--- src/componentes/separadorOlasSirena.js
+++ src/componentes/separadorOlasSirena.js
@@ -3,7 +3,7 @@ import React from 'react'
 function SeparadorOlasSirena() {
     return (
         <div>
-            <img className="ol-mg img-fluid" alt="sirena" src="https://res.cloudinary.com/agencia-web-refresh/image/upload/v1643851787/lila_hvhjmi_e62228474c.webp"></img>
+            <img className="ol-mg img-fluid" width={1920} height={1080} alt="sirena" src="https://res.cloudinary.com/agencia-web-refresh/image/upload/v1643851787/lila_hvhjmi_e62228474c.webp"></img>
         </div>
     )
 }

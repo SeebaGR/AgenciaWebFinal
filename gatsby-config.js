ed166f55351969456fd9511317d47f69669bd075
SeebaGR <72950404+SeebaGR@users.noreply.github.com>
2022-06-13 16:55:42 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,30 +41,10 @@ module.exports = {
           resolve: "gatsby-plugin-google-tagmanager",
           options: {
             id: "GTM-NR96Z4S",
-      
-            // Include GTM in development.
-            //
-            // Defaults to false meaning GTM will only be loaded in production.
             includeInDevelopment: false,
-      
-            // datalayer to be set before GTM is loaded
-            // should be an object or a function that is executed in the browser
-            //
-            // Defaults to null
             defaultDataLayer: { platform: "gatsby" },
-      
-            // Specify optional GTM environment details.
-      
-      
-            // Name of the event that is triggered
-            // on every Gatsby route change.
-            //
-            // Defaults to gatsby-route-change
-            routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
-            // Defaults to false
             enableWebVitalsTracking: true,
-            // Defaults to https://www.googletagmanager.com
-            selfHostedOrigin: "YOUR_SELF_HOSTED_ORIGIN",
+
           },
         },
         {

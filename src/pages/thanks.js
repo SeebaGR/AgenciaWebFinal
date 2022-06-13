853bb428c0163b6c933308edecdc9756965cf7cf
SeebaGR <72950404+SeebaGR@users.noreply.github.com>
2022-06-13 09:48:39 -0400

    Create thanks.js

---
+++ src/pages/thanks.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "../componentes/header"
+import Footer from "../componentes/footer"
+import SectionInstagram from "../componentes/SectionInstagram"
+import SectionSaberMas from "../componentes/componentes_home/sectionSaberMas";
+import SectionGracias from "../componentes/componente_contacto/SectionBannerGracias";
+
+function thanks() {
+    const colorFondo = "#031fff"
+
+    return (
+        <div>
+            <Header colorFondo={colorFondo}></Header>
+            <SectionGracias></SectionGracias>
+            <SectionSaberMas></SectionSaberMas>
+            <SectionInstagram></SectionInstagram>
+            <Footer></Footer>
+        </div>
+    )
+}
+
+export default thanks

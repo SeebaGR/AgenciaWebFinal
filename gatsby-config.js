57852f303551bddfdb14f5f2b1a187c9394f9c1a
SeebaGR <72950404+SeebaGR@users.noreply.github.com>
2022-06-14 11:52:51 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,42 @@ module.exports = {
               singleTypes: [`home`,`mkt`,`tripulacion`,`analitica`],
             },
           },
- 
+          {
+            resolve: "gatsby-plugin-google-tagmanager",
+            options: {
+              id: "GTM-MGR9BD4",
+              includeInDevelopment: false,
+              defaultDataLayer: { platform: "gatsby" },
+              enableWebVitalsTracking: true,
+  
+            },
+          },
+          {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+              // You can add multiple tracking ids and a pageview event will be fired for all of them.
+              trackingIds: [
+                "UA-65758649-3", // Google Analytics / GA
+              ],
+  
+              gtagConfig: {
+                optimize_id: "OPT_CONTAINER_ID",
+                anonymize_ip: true,
+                cookie_expires: 0,
+              },
+   
+              pluginConfig: {
+                // Puts tracking script in the head instead of the body
+                head: false,
+                // Setting this parameter is also optional
+                respectDNT: true,
+                // Avoids sending pageview hits from custom paths
+                exclude: ["/preview/**", "/do-not-track/me/too/"],
+                // Defaults to https://www.googletagmanager.com
+                origin: "YOUR_SELF_HOSTED_ORIGIN",
+              },
+            },
+          },
        
        
     ]

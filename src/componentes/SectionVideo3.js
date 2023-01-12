90d1daf1243ca4b20c2caa1a8eeadb633f53a306
SeebaGR <mejorandotuweb2022@gmail.com>
2023-01-12 11:25:34 -0300

    Update SectionVideo3.js

--- src/componentes/SectionVideo3.js
+++ src/componentes/SectionVideo3.js
@@ -19,80 +19,10 @@ export const SectionVideo3 = (props) => {
 
   return (
     <div className="padd-padd" style={{  paddingTop: "40px", paddingBottom: "40px", background: props.colorFondo }}>
-      <div
-        className="modal-video"
-        style={{
-          width:"712px",
-          display:"none",
-          alignItems:"center",
-          justifyContent:"center",
-          margin:"0 auto",
-          textAlign: "center",
-          backgroundSize: "contain",
-          backgroundRepeat: "no-repeat ",
-          backgroundPosition: "center",
-          height: "400px",
-          backgroundImage: {vidio},
-        }}
-      >
-        <Button
-          variant="primary"
-          style={{
-            
-            
-            background: "transparent",
-            border: "none",
-          }}
-          onClick={() => {
-            setShow(!show);
-          }}
-        >
-     
-        </Button>
-      </div>
-    
-
       
-      {show ? (
-             <div
-             className="modal-video videocapitan"
-             
-
-          
-
-           >
-<img loading="lazy"  className="ol-mg img-fluid" alt="olas" width="100%"  src="https://cdn.shopify.com/s/files/1/0654/9366/3973/files/Thumbnail-01_1.jpg?v=1660746182"></img>
-           
-             <Button
-               variant="primary"
-               style={{
-                 height:"109px",
-                 width:"109px",
-                 position:"absolute",
-                 background: "transparent",
-                 border: "none",
-               }}
-               onClick={() => {
-                 setShow(!show);
-               }}
-             >
-           
-             </Button>
-           </div>
-      ) : (
-        <div style={{display:"flex", justifyContent:"center"}}>
       <iframe className="lazyframe videoiframe2" width="778" height="400" src="https://player.vimeo.com/video/783934764?h=f936b1d278&title=0&byline=0&portrait=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <iframe className="lazyframe videoiframe1" width="380" height="270" src="https://player.vimeo.com/video/783934764?h=f936b1d278&title=0&byline=0&portrait=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-      </div>
-        
-      )}
-   
-         
-
-
-      
-       
    
     </div>
   );

1d719188a99e4ce62463ea9cfa2a42692cf56da0
SeebaGR <mejorandotuweb2022@gmail.com>
2023-01-12 11:28:09 -0300

    Update SectionVideo3.js

--- src/componentes/SectionVideo3.js
+++ src/componentes/SectionVideo3.js
@@ -18,7 +18,7 @@ export const SectionVideo3 = (props) => {
 
 
   return (
-    <div className="padd-padd" style={{  paddingTop: "40px", paddingBottom: "40px", background: props.colorFondo }}>
+    <div className="padd-padd" style={{ textAlign:"center",  paddingTop: "40px", paddingBottom: "40px", background: props.colorFondo }}>
       
       <iframe className="lazyframe videoiframe2" width="778" height="400" src="https://player.vimeo.com/video/783934764?h=f936b1d278&title=0&byline=0&portrait=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <iframe className="lazyframe videoiframe1" width="380" height="270" src="https://player.vimeo.com/video/783934764?h=f936b1d278&title=0&byline=0&portrait=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>

9b73436d8d2555076c64dba0a2137a711ee1120e
SeebaGR <72950404+SeebaGR@users.noreply.github.com>
2023-04-10 12:07:03 -0400

    Update SectionVideo.js

--- src/componentes/SectionVideo.js
+++ src/componentes/SectionVideo.js
@@ -81,8 +81,8 @@ export const SectionVideo = (props) => {
            </div>
       ) : (
         <div style={{display:"flex", justifyContent:"center"}}>
-      <iframe className="lazyframe videoiframe2" width="778" height="400" src="https://www.youtube.com/embed/Yzhh1GJjACE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <iframe className="lazyframe videoiframe1" width="380" height="270" src="https://www.youtube.com/embed/Yzhh1GJjACE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe className="lazyframe videoiframe2" width="778" height="400" src="https://www.youtube.com/embed/kMPNdhu54xc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe className="lazyframe videoiframe1" width="380" height="270" src="https://www.youtube.com/embed/kMPNdhu54xc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
       </div>
         

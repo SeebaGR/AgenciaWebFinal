f4aa8871175f7c6a9c54b1f67395afac57aefd81
SeebaGR <72950404+SeebaGR@users.noreply.github.com>
2023-06-14 16:28:56 -0400

    Update seccion_mkt2.js

--- src/componentes/componentes_mkt/seccion_mkt2.js
+++ src/componentes/componentes_mkt/seccion_mkt2.js
@@ -23,7 +23,7 @@ const seccionMkt2 = () => {
           <div className="fortwo"  >
           <h1 className="parru-mkt">¿TUS RESULTADOS NO SON LOS QUE ESPERAS?</h1>
           <h1 className="parru-mkt2">Nos especializamos por crear estrategias digitales de alto impacto en Facebook e Instagram Ads, Google Ads, eMail Marketing y SEO.</h1>
-          <h1 className="parru-mkt3">Hemos administrado más de 7 millones de dolares en pauta publicitaria y hemos asesorado a +500 ecommerce en la implementacion de campañas de marketing digital con foco a performance.</h1>
+          <h1 className="parru-mkt3">Hemos administrado más de 10 millones de dolares en pauta publicitaria y hemos asesorado a +700 ecommerce en la implementacion de campañas de marketing digital con foco a performance.</h1>
           <h1 className="parru-mkt4">Actualmente formamos parte de Google Partners Premier y somos una de las empresas destacadas en su programa de aceleradoras de agencias en Latam.</h1>
           <a
               

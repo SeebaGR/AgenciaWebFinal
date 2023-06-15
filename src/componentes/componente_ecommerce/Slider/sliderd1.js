9c2fb86235b4cb87f0faf72de3a5865e68670d00
SeebaGR <72950404+SeebaGR@users.noreply.github.com>
2023-06-15 16:55:19 -0400

    s

---
+++ src/componentes/componente_ecommerce/Slider/sliderd1.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { Swiper, SwiperSlide} from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "../../global-styles/estilo.scss"
+import SwiperCore, { Navigation, Autoplay, Pagination } from "swiper";
+
+
+// install Swiper modules
+SwiperCore.use([Navigation]);
+SwiperCore.use([Autoplay]);
+const sliderd1 = () => {
+ 
+  return (
+    <div  >
+
+
+ 
+      <Container>
+        <Row style={{ alignItems: "center", justifyContent: "center" }}>
+  
+          <Col >
+            <Swiper style={{paddingLeft:"20px", paddingRight:"20px"}} 
+              slidesPerView={4}
+              spaceBetween={10}
+              navigation={true} modules={[Navigation, Pagination]}   
+              autoplay={{
+                delay: 1500
+            }}
+              className="mySwiperSlider"
+              breakpoints={{
+                "360": {
+                  "slidesPerView": 1,
+                  "spaceBetween": 20
+                },
+                "400": {
+                  "slidesPerView": 1,
+                  "spaceBetween": 20
+                },
+                "640": {
+                  "slidesPerView": 1,
+                  "spaceBetween": 20
+                },
+                "768": {
+                  "slidesPerView": 1,
+                  "spaceBetween": 40
+                },
+                "1024": {
+                  "slidesPerView": 4,
+                }
+              }}
+              
+            >
+              {Datos.map((item, i) => (
+                <SwiperSlide key={i}>
+                  <a href={item.link1} key={i}>
+                    <img loading="lazy" 
+                      key={i}
+                      className="imsld-fin img-slider-movil img-fluid"
+                      width={203}
+                      height={150}
+                      src={item.imagen1[0].url}
+                      alt=""
+                    ></img>
+                  </a>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </Col>
+        </Row>
+
+      </Container>
+    </div>
+  );
+};
+
+export default sliderd1;

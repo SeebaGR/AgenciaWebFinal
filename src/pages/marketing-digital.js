a386baa7cc6fae7541c106de67686ee8ca952d3b
SeebaGR <72950404+SeebaGR@users.noreply.github.com>
2023-06-15 17:19:03 -0400

    Update marketing-digital.js

--- src/pages/marketing-digital.js
+++ src/pages/marketing-digital.js
@@ -1,50 +1,33 @@
 import React from "react";
+import Sct1 from "../componentes/componente_marketing/section_bannerm1"
+import Sct2 from "../componentes/componente_marketing/section_m2"
+import Sct3 from "../componentes/componente_marketing/section_m3"
+import Sct4 from "../componentes/componente_marketing/section_m4"
+import Sct5 from "../componentes/componente_marketing/section_m5"
+import Sct6 from "../componentes/componente_marketing/section_m6"
+import Footer from "../componentes/componente_marketing/footerd"
 import Header from "../componentes/header"
-import Footer from "../componentes/componentes_desarrollo/footerd"
-import Sectionbanner from "../componentes/componentes_mkt/section_banner"
-import Seccionmkt3 from "../componentes/componentes_mkt/seccion_mkt3"
-import SeccionMkt2 from "../componentes/componentes_mkt/seccion_mkt2"
-import Slider from "../componentes/componentes_home/slider"
-import SectionContacto from "../componentes/SectionContacto"
-import SectionInstagram from "../componentes/SectionInstagram";
-import "../componentes/global-styles/layout"
-import Seo from "../componentes/seo"
-import Seccionmkt4 from "../componentes/componentes_mkt/seccion_mkt4"
-import Seccionmkt5 from "../componentes/componentes_mkt/seccion_mkt5"
-import Seccionmkt6 from "../componentes/componentes_mkt/seccion_mkt6"
-import Seccionmkt7 from "../componentes/componentes_mkt/seccion_mkt7"
+import Seo from "../componentes/componente_marketing/seo"
 
-
-
-function marketingdigital() {
-  
+// markup
+function Ecommerce() {
   return (
-    <div>
-      <Seo
-              title="Marketing Digital, Google Adwords y Facebook Ads"
+    <React.Fragment >
+         <Seo
+              title="Agencia de Marketing Digital Chile | Meta Ads, Google Ads, eMail Marketing."
   
             ></Seo>
-      <Header id="mktmkt" ></Header>
-      <Sectionbanner></Sectionbanner>
-      <SeccionMkt2></SeccionMkt2>
-      <Seccionmkt3></Seccionmkt3>
-      <div style={{background:"#91D2D2", height:"20px"}}></div >
-      <Seccionmkt4></Seccionmkt4>
-      <div style={{background:"#5D57DD", height:"20px"}}></div >
-      <Seccionmkt5></Seccionmkt5>
-    
-      <Seccionmkt7></Seccionmkt7>
-    
-      <Footer />
-
-      
-
-
-
-
-
-    </div>
-  );
+                        
+      <Header ></Header>
+      <Sct1 ></Sct1>
+      <Sct2 ></Sct2>
+      <Sct3></Sct3>
+      <Sct4></Sct4>
+      <Sct5></Sct5>
+      <Sct6></Sct6>
+      <Footer></Footer>
+    </React.Fragment>
+  )
 }
 
-export default marketingdigital;
+export default marketingdigital

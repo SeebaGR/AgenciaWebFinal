1dfaaf063aaa1b46bede9949f91237cbc07b431a
SeebaGR <72950404+SeebaGR@users.noreply.github.com>
2023-08-04 12:13:47 -0400

    Update prensa.js

--- src/pages/prensa.js
+++ src/pages/prensa.js
@@ -5,12 +5,18 @@ import Seccion2 from "../componentes/componente_prensa/seccion_blog_prensa"
 import Seccion3 from "../componentes/componente_prensa/section_3"
 import Header from "../componentes/header"
 import Footer from "../componentes/footer"
-import Seccion4 from "../componentes/componente_prensa/seccion_importantes"
+import Seo from "../componentes/seo"
 const prensas = () => {
   const colorFondo= "#031fff"
   
     return (
       <React.Fragment>
+              <Seo
+              title="Artículos de Prensa | Agencia los Navegantes
+              "
+              description="Conoce más sobre Agencia Los Navegantes a través de artículos de prensa,  entrevistas e invitaciones a canales de YouTube.
+              "
+            ></Seo>
         <Header  style={{backgroundColor:"red"}}/>
         <Seccion1></Seccion1>
         <Seccion2></Seccion2>

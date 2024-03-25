0c42c8568418a5ebba1a4f17fdae9a6b61584a62
SeebaGR <72950404+SeebaGR@users.noreply.github.com>
2024-03-25 14:26:56 -0300

    s

--- src/componentes/componentes_home/slidertesti.js
+++ src/componentes/componentes_home/slidertesti.js
@@ -12,7 +12,7 @@ const slidertesti = () => {
   return (
    
 <div style={{width:"1300px", margin:"0 auto", marginTop:"30px"}}>
-    <div style={{marginBottom:"30px"}}><iframe src="https://player.vimeo.com/video/926398171?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" className="tititu2"   frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write"   title="Bárbara Momberg"></iframe><script src="https://player.vimeo.com/api/player.js"></script>
+    <div style={{textAlign:"center",marginBottom:"30px"}}><iframe src="https://player.vimeo.com/video/926398171?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" className="tititu2"   frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write"   title="Bárbara Momberg"></iframe><script src="https://player.vimeo.com/api/player.js"></script>
  </div>
 
 <Row>

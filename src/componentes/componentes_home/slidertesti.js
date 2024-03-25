2e5d67db55f2526db5d819c1f2453c65632a3bd7
SeebaGR <72950404+SeebaGR@users.noreply.github.com>
2024-03-25 15:27:38 -0300

    s

--- src/componentes/componentes_home/slidertesti.js
+++ src/componentes/componentes_home/slidertesti.js
@@ -12,6 +12,7 @@ const slidertesti = () => {
   return (
    
 <div className="forttt">
+<h1 style={{marginTop:"50px",marginBottom:"40px"}}    className="somospTitulo  encabezado">Resultados de empresas que confían en nosotros 🚀</h1>
     <div style={{textAlign:"center",marginBottom:"30px"}}><iframe src="https://player.vimeo.com/video/926398171?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" className="tititu2"   frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write"   title="Bárbara Momberg"></iframe><script src="https://player.vimeo.com/api/player.js"></script>
  </div>
 

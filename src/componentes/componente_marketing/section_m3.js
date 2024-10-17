f6a097e3160f6c4a58e073828f9655a523568705
SeebaGR <72950404+SeebaGR@users.noreply.github.com>
2024-10-17 18:08:45 -0300

    Update section_m3.js

--- src/componentes/componente_marketing/section_m3.js
+++ src/componentes/componente_marketing/section_m3.js
@@ -570,7 +570,7 @@ REQUIERE UN TRABAJO CONSTANTE</h1>
           width="100%"
         key="id1"
         className="lazyload img "
-        src="https://cdn.shopify.com/s/files/1/0610/5648/3573/files/collage_c94269f8-7bec-427e-aa4f-44ec5fd84790.png?v=1727357057"
+        src="https://cdn.shopify.com/s/files/1/0610/5648/3573/files/collage_1_a0a4d4fb-55c3-47f5-be8d-63be28a779db.png?v=1729198665"
       ></img>
 
     </Col>

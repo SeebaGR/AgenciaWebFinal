97829b48c6d0ae0a0dbeacb9ccbe7a65507f4fc3
SeebaGR <72950404+SeebaGR@users.noreply.github.com>
2025-02-10 12:05:16 -0300

    Update sliderReviews.js

--- src/componentes/componente_marketing/Slider/sliderReviews.js
+++ src/componentes/componente_marketing/Slider/sliderReviews.js
@@ -221,6 +221,15 @@ alt=""
 
 alt=""
   className=" mmimm lazyload img-fluid "
+  src="https://cdn.shopify.com/s/files/1/0610/5648/3573/files/AN_copia_2.png?v=1739199410"
+
+></img>
+</SwiperSlide>
+            
+ <SwiperSlide>  <img
+
+alt=""
+  className=" mmimm lazyload img-fluid "
   src="https://cdn.shopify.com/s/files/1/0610/5648/3573/files/AGENCIA_-_Testimonios_AN-21.webp?v=1719866957"
 
 ></img>
